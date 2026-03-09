5662a77e5a0a0460e10c51684bf08c8757bf9ced
carry2on <manankpandya307@gmail.com>
2026-03-09 14:46:36 +0530

    reguster form work is donne now made product pages

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('abouts/',views.about,name='about'),
     path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
-    
+    path('register/',views.register_user,name='register'), 
+    path('product/<int:pk>',views.product,name='product')
+
 ]
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,12 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product
 from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 def home(req):
     products=Product.objects.all()
     return render(req,'home.html',{'products':  products})
 def about(req):
     return render(req,'about.html')
 def login_user(req):
-    return render(req,'login.html')
+    if req.method == 'POST':
+        username = req.POST['username']
+        password = req.POST['password']
+
+        user = authenticate(req, username=username, password=password)
+
+        if user is not None:
+            login(req, user)
+            messages.success(req, "You are successfully logged in")
+            return redirect('home')
+        else:
+            messages.error(req, "Invalid username or password")
+            return redirect('login')
+
+    return render(req, 'login.html')
 def logout_user(req):
-    return render(req,'home.html')
+    logout(req)
+    messages.success(req,('you ae sucessfully loged out '))
+    return redirect('home')
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import SignUpForm
+def product(req,pk):
+    product=Product.objects.get(id=pk)
+    return render(req,'product.html',{'product':product})
+
+def register_user(req):
+    if req.method == "POST":
+        form = SignUpForm(req.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            user = authenticate(req, username=username, password=password)
+            login(req, user)
+
+            messages.success(req, "Account created successfully!")
+            return redirect("home")
+
+    else:
+        form = SignUpForm()
+
+    return render(req, "register.html", {"form": form})
